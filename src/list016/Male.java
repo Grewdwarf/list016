59eaf05d57e3c72d190977e100811b691ce01336
Love Gruvberg <Grooveberg@Loves-MacBook-Pro.local>
2021-03-09 10:36:16 +0100

    Created classes; Male and Person

---
+++ src/list016/Male.java
@@ -0,0 +1,14 @@
+package list016;
+
+public class Male extends Person {
+	public static final String CLASS_NAME = "Male";
+	
+	public Male(String name, int age) {
+		this.name = name;
+		this.age = age;	
+	}
+	
+	public void printClassName() {
+		System.out.println();
+	}
+}
